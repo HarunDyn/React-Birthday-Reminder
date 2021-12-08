d4869f7307b6804c8e35cbcc6912d43da9ce5bbf
Harun Dayan <harundayann@gmail.com>
2021-12-08 16:10:04 +0300

    reminder

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 0,
+    name: "Harun Dayan",
+    age: 29,
+    image:
+      "https://media-exp1.licdn.com/dms/image/C4E03AQHctw6t9bM8sg/profile-displayphoto-shrink_800_800/0/1630446877463?e=1644451200&v=beta&t=OKRfdFFjoJiLP6zYDjS3atYLp800TeEF3bj64DwpKbo",
+  },
+  {
+    id: 1,
+    name: "Bertie Yates",
+    age: 29,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+  },
+  {
+    id: 2,
+    name: "Hester Hogan",
+    age: 32,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg",
+  },
+  {
+    id: 3,
+    name: "Larry Little",
+    age: 36,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+  },
+  {
+    id: 4,
+    name: "Sean Walsh",
+    age: 34,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+  },
+  {
+    id: 5,
+    name: "Lola Gardner",
+    age: 29,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+  },
+];
